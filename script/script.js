fbbcadd3c8a2a87a22f4d82f3a2418b247a2c961
Josh <malmmlam@outlook.com>
2022-01-30 21:53:31 +0100

    add display update feature

--- script/script.js
+++ script/script.js
@@ -23,3 +23,31 @@ function operate(num1, num2, operator) {
 }
 
 console.log(operate(10, 2, '/'));
+
+// const key = document.querySelector('.clear');
+// key.addEventListener('click', myFunction);
+
+function myFunction() {
+    display.textContent = key.textContent;
+}
+
+const displayKeys = document.querySelectorAll('.display-key');
+const display = document.querySelector('#display');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
+
+displayKeys.forEach(key => key.addEventListener('click', updateDisplay));
+clear.addEventListener('click', clearDisplay);
+
+
+function updateDisplay(key) {
+    console.log(key.currentTarget.textContent);
+    display.textContent += key.currentTarget.textContent;
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+
+
